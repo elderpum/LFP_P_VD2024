e2ff20ba3c3cc8cc83bd4214cc220218b5699717
Elder Pum <ElderPum@gmail.com>
2024-12-27 20:01:55 -0600

    Front de Angular estilizado

--- Ejemplo_Angular/src/app/app.component.ts
+++ Ejemplo_Angular/src/app/app.component.ts
@@ -8,6 +8,7 @@ import { ApiService } from './api/api.service';
 })
 export class AppComponent {
   codigo: string = '';
+  consola: string = '';
   lexemas: any[] = [];
   errores: any[] = [];
 
@@ -23,11 +24,31 @@ export class AppComponent {
       (res: { lexemas: any[]; errores: any[] }) => {
         this.lexemas = res.lexemas || [];
         this.errores = res.errores || [];
+        this.imprimirEnConsola();
       },
       (err: any) => {
         console.error('Error al analizar el código:', err);
         alert('Hubo un error al comunicarse con la API.');
       }
     );
+  }
+
+  limpiar() {
+    this.codigo = '';
+    this.consola = '';
+    this.lexemas = [];
+    this.errores = [];
+  }
+
+  imprimirEnConsola() {
+    // Imprimir los lexemas y los errores en la consola
+    this.consola = '';
+    console.log('Lexemas:', this.lexemas);
+    this.lexemas.forEach((lexema) => {
+      this.consola += `${lexema.tipo} - (${lexema.valor})\n`;
+    });
+    this.errores.forEach((error) => {
+      this.consola += `${error}\n`;
+    });
   }
 }
